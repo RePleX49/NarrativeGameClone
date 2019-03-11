4edefc04cab10f874b7a6b2a80d514434cd6738e
RePleX49 <lukelin2009@hotmail.com>
2019-03-11 01:29:50 -0400

    Initial commit

---
+++ Assets/Scripts/AdventureGameController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGameController : MonoBehaviour {
+
+    public Text textComponent; //SerializeField makes variable editable in inspector
+    public Text TextOption1;
+    public Text TextOption2;
+    public State startingState;
+
+    State state;
+
+	// Use this for initialization
+	void Start () {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        TextOption1.text = state.GetOption1();
+        TextOption2.text = state.GetOption2();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if(nextStates.Length != 0)
+        {
+            state = nextStates[0];
+
+            // Update text from state
+            textComponent.text = state.GetStateStory();
+            TextOption1.text = state.GetOption1();
+            TextOption2.text = state.GetOption2();
+        }        
+    }
+
+    public void SelectOption()
+    {
+        ManageState();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Now deriving from ScriptableObject class
+//CreateAssetMenu creates a tab for this class in the Create contextual tab
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] string Option1;
+    [SerializeField] string Option2;
+    [SerializeField] State[] nextStates;
+	
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+    public string GetOption1()
+    {
+        return Option1;
+    }
+
+    public string GetOption2()
+    {
+        return Option2;
+    }
+}
